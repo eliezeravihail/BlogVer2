ad77b1cfb4ab1cadadb489a358fd8504a1377b83
eliezer avihail <eliezeravihail@gmail.com>
2021-01-07 00:05:31 +0200

    add old posts need fix it

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace BlogVer2
 
 
 
-            services.AddSession(options => { options.IdleTimeout = TimeSpan.FromMinutes(1); });
+            services.AddSession(options => { options.IdleTimeout = TimeSpan.FromMinutes(20); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
