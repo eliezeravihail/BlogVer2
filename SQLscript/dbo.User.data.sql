fedacb654e2e287f4fae58dc8d7ec2832e714e0e
eliezer avihail <eliezeravihail@gmail.com>
2021-01-17 15:28:37 +0200

    fix css and remove dorder to keep the consistency

---
+++ SQLscript/dbo.User.data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[User] ON
+INSERT INTO [dbo].[User] ([ID], [Name], [Password]) VALUES (2, N'אליעזר', N'1234')
+INSERT INTO [dbo].[User] ([ID], [Name], [Password]) VALUES (4, N'aharon', N'1212')
+INSERT INTO [dbo].[User] ([ID], [Name], [Password]) VALUES (5, N'youel', N'321')
+INSERT INTO [dbo].[User] ([ID], [Name], [Password]) VALUES (6, N'daivid', N'qqwe13')
+SET IDENTITY_INSERT [dbo].[User] OFF
